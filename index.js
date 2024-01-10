1819aed720b0a88552910eab83965cc4f08a08ed
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-10 11:51:14 +0300

    rggrgr

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const app = express()
 
+
+app.use(express.static("static_files"))
+
+
 app.get('/', function (req, res) {
-  res.send('Hello pppppWold')
+  res.send('Hello World')
 })
 
-app.listen(3000)
+app.get('/A9', function (req, res) {
+  res.send('9pip9')
+})
+
+app.get('/hello/:name', function (req, res) {
+  res.send('hello ' + req.params.name)
+})
+
+
+console.log("сервер работает")
+
+
+app.listen(3500)
