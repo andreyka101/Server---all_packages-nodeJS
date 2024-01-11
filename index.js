47cfe6753b581efb40def16939a8be3d0d2141d3
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-11 19:28:27 +0300

    аиааиаи

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-// загрузка модулей
+// подключение модулей
 import express from 'express'
-// const express = require('express')
 import compression from 'compression'
-// const compression = require('compression')
+import cors from 'cors'
+
+
 const app = express()
+
 
 // включение compression
 // почему то не работает
-app.use(compression({strategy:8}))
+app.use(compression({strategy:3}))
+
+
+
+// включение cors
+app.use(cors())
+
+
 
 
 app.use(express.static("static_files"))
@@ -16,13 +25,10 @@ app.use(express.static("static_files"))
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-
-
+// http://localhost:3500/A9
 app.get('/A9', function (req, res) {
   res.send('9pip9')
 })
-
-
 app.get('/hello/:name', function (req, res) {
   res.send('hello ' + req.params.name)
 })
