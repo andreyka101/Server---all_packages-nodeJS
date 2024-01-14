6ce879bcebdb30bc143c669d1d636e031c9160f1
andreyka101 <andrejkvasnin236@gmail.com>
2024-01-14 19:57:56 +0300

    пипипи

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ const app = express()
 
 //REVIEW - включение compression
 //FIXME - почему то не работает
-// опаче инджинкс
-app.use(compression({strategy:3}))
+app.use(compression({strategy:8}))
 
 
 
-//FIXME - ???
 //REVIEW - включение cors
-app.use(cors({origin:["http://localhost:5173",'http://127.0.0.1:5173']}))
+app.use(cors())
+// пример запроса только с моего домена
+// app.use(cors({origin:["http://localhost:5173",'http://127.0.0.1:5173']}))
 
 
 
